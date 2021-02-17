a363b43d4e4947dd8e3a5102d5dfb5c59746c5ba
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-02-17 17:45:48 +0300

    feat: eslint, husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Me from './pages/Me/Me';
 import {
   BrowserRouter as Router,
   Redirect,
   Route,
 } from 'react-router-dom';
+import Me from './pages/Me/Me';
 
 const App = () => (
   <Router>
