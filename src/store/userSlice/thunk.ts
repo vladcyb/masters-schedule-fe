34a70fe0b4c987e3b643574e47bc99d6ee5f72dd
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-02-18 21:43:40 +0300

    fix: При отправке формы ошибки пропадают

--- src/store/userSlice/thunk.ts
+++ src/store/userSlice/thunk.ts
@@ -7,6 +7,7 @@ const UserThunk = {
   login: createAsyncThunk(
     'user/login',
     async (props: ILoginProps, { dispatch }) => {
+      props.setters.setErrors({});
       const result = await API.User.login(props);
       const { ok, error } = result.data;
       if (ok) {
