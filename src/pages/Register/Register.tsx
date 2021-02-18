6602f5ee3e1a988992f05a6de3fc0f00b515956d
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-02-18 18:59:06 +0300

    feat: Добавил состояние в форму регистрации

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -2,16 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Field } from '../../components';
 import './style.css';
+import { useField } from '../../shared/hooks/useField';
 
 export const Register = () => {
-  console.log(123);
+  /* hooks */
+  const login = useField();
+  const password = useField();
+  const passwordRepeat = useField();
+
+  /* vars */
+  const formData = {
+    login: login.props.value,
+    password: password.props.value,
+    passwordRepeat: passwordRepeat.props.value,
+  };
+
+  /* methods */
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <div className="register">
-      <Field label="Login:" name="login" />
-      <Field label="Password:" name="password" />
-      <Field label="Repeat password:" name="repeatedPassword" />
-      <Button className="register__submit">Register</Button>
+    <form className="register" onSubmit={handleSubmit} autoComplete="off">
+      <Field label="Login:" name="login" {...login.props} />
+      <Field label="Password:" name="password" {...password.props} />
+      <Field label="Repeat password:" name="repeatedPassword" {...passwordRepeat.props} />
+      <Button className="register__submit" type="submit">Register</Button>
       <Link className="navlink register__login" to="/login">Login</Link>
-    </div>
+    </form>
   );
 };
