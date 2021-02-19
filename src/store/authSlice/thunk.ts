ae31e4264a81908c4d63bab809ca29b320bf5921
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-02-19 11:55:45 +0300

    feat: Отображение ошибок с бэка в форме регистрации

--- src/store/authSlice/thunk.ts
+++ src/store/authSlice/thunk.ts
@@ -7,22 +7,29 @@ const UserThunk = {
   login: createAsyncThunk(
     'auth/login',
     async ({ setters, ...props }: ILoginAPI, { dispatch }) => {
-      setters.setErrors({});
+      const { setErrors } = setters;
+      setErrors({});
       const result = await API.User.login(props);
       const { ok, error } = result.data;
       if (ok) {
         dispatch(actions.login());
       } else {
-        setters.setErrors(error);
+        setErrors(error);
       }
     },
   ),
   register: createAsyncThunk(
     'auth/register',
     async ({ setters, ...props }: IRegisterAPI) => {
-      setters.setErrors({});
+      const { setErrors } = setters;
+      setErrors({});
       const result = await API.User.register(props);
-      console.log(result);
+      const { ok, error } = result.data;
+      if (ok) {
+        console.log('ok'); // TODO
+      } else {
+        setErrors(error);
+      }
     },
   ),
 };
