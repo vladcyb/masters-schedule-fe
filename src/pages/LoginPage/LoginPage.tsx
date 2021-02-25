5d9a51b0e6036b961dd5089540868e5366e8f8c6
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-02-25 17:50:23 +0300

    fix: LoginPage and RegisterPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -4,6 +4,7 @@ import './style.css';
 
 export const LoginPage = () => (
   <div className="loginPage">
+    <div className="title center">Login</div>
     <LoginForm className="loginPage__form" />
   </div>
 );
--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -4,6 +4,7 @@ import './style.css';
 
 export const RegisterPage = () => (
   <div className="registerPage">
+    <div className="center title">Register</div>
     <RegisterForm className="registerPage__form" />
   </div>
 );
