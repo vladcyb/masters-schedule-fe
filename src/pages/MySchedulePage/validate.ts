8bf35221452165bcbae627185131d60b6d2c45b5
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-03-05 14:40:27 +0300

    fix: валидцация

--- src/pages/MySchedulePage/validate.ts
+++ src/pages/MySchedulePage/validate.ts
@@ -2,7 +2,10 @@ import { Setters } from '../../shared/hooks/useSetters/types';
 
 export const validateSetSchedule = (hours: string, setters: Setters): boolean => {
   const errors: any = {};
-  if (!(/^([([0-1]?[0-9]|2[0-3]):[0-5][0-9]-([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/).test(hours)) {
+  if (
+    !(/^([([0-1]?[0-9]|2[0-3]):[0-5][0-9]-([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/)
+      .test(hours.replace(/\s/g, ''))
+  ) {
     errors.hours = 'Hours must be in format `hh:mm-hh:mm`';
   }
   setters.setErrors(errors);
