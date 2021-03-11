c5dcb4d136635dd0497a81f4c735117ba527cab8
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-03-11 18:13:24 +0300

    refactor: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,24 @@
 import React, { useEffect } from 'react';
-import { Redirect, Route, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { thunks } from './store/thunks';
 import { getUser } from './store/userSlice/selectors';
-import { PrivateRoute } from './HOCs';
-import {
-  LocationsPage,
-  LoginPage,
-  Me,
-  MySchedulePage,
-  OrdersPage,
-  RegisterPage,
-  ServicesPage,
-  SpecializationsPage,
-} from './pages';
 import { useAppDispatch } from './store';
-import { routes } from './shared/routes';
 import { UserRole } from './API/interfaces';
 import { Navbar } from './components/ui';
 import { RolesMap } from './shared/types';
+import { Routes } from './components/Routes';
 import './app.css';
 
 const App = () => {
   /* loading user data from Redux */
+  const user = useSelector(getUser);
   const {
     data: userData,
     fetched: isFetched,
-    loading: isLoading,
-    orders,
-  } = useSelector(getUser);
+  } = user;
 
   /* hooks */
   const dispatch = useAppDispatch();
-  const location = useLocation();
-  const prevPath = location.state ? (location.state as any).prevPath : undefined;
 
   /* effects */
   useEffect(() => {
@@ -55,8 +40,6 @@ const App = () => {
     isResponsible: role === UserRole.RESPONSIBLE,
   };
 
-  const isLoggedIn = Boolean(userData.login);
-
   return (
     <div className="app">
       {isFetched ? (
@@ -64,77 +47,7 @@ const App = () => {
           {userData.login && (
             <Navbar login={userData.login} onLogout={handleLogout} rolesMap={rolesMap} />
           )}
-          <Route path="/" exact>
-            <Redirect to={routes.me.root} />
-          </Route>
-          <PrivateRoute
-            path={routes.me.root}
-            exact
-            condition={isLoggedIn}
-            redirectPath={routes.login.root}
-          >
-            <Me userData={userData} />
-          </PrivateRoute>
-          <PrivateRoute
-            path={routes.login.root}
-            exact
-            condition={!isLoggedIn}
-            redirectPath={prevPath || routes.me.root}
-          >
-            <LoginPage isLoading={isLoading} />
-          </PrivateRoute>
-          <PrivateRoute
-            path={routes.orders.root}
-            exact
-            condition={isLoggedIn}
-            redirectPath={routes.login.root}
-          >
-            <OrdersPage orders={orders} />
-          </PrivateRoute>
-          <PrivateRoute
-            path={routes.register.root}
-            exact
-            condition={!isLoggedIn}
-            redirectPath={routes.me.root}
-          >
-            <RegisterPage isLoading={isLoading} />
-          </PrivateRoute>
-          <PrivateRoute
-            path={routes.locations.root}
-            redirectPath={routes.login.root}
-            condition={isLoggedIn}
-            allowedRoles={[UserRole.ADMIN]}
-            role={role}
-          >
-            <LocationsPage />
-          </PrivateRoute>
-          <PrivateRoute
-            path={routes.services.root}
-            condition={isLoggedIn}
-            redirectPath={routes.login.root}
-            allowedRoles={[UserRole.ADMIN]}
-            role={role}
-          >
-            <ServicesPage />
-          </PrivateRoute>
-          <PrivateRoute
-            path={routes.specializations.root}
-            condition={isLoggedIn}
-            redirectPath={routes.login.root}
-            allowedRoles={[UserRole.ADMIN]}
-            role={role}
-          >
-            <SpecializationsPage />
-          </PrivateRoute>
-          <PrivateRoute
-            path={routes.schedule.root}
-            condition={isLoggedIn}
-            redirectPath={routes.login.root}
-            allowedRoles={[UserRole.ADMIN, UserRole.OPERATOR, UserRole.MASTER]}
-            role={role}
-          >
-            <MySchedulePage />
-          </PrivateRoute>
+          <Routes user={user} />
         </>
       ) : null}
     </div>
---
+++ src/components/Routes/Routes.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import {
+  Redirect, Route, useLocation, Switch,
+} from 'react-router-dom';
+import { routes } from '../../shared/routes';
+import { PrivateRoute } from '../../HOCs';
+import {
+  LocationsPage,
+  LoginPage,
+  Me, MySchedulePage,
+  OrdersPage,
+  RegisterPage,
+  ServicesPage,
+  SpecializationsPage,
+} from '../../pages';
+import { UserRole } from '../../API/interfaces';
+import { StateType as UserState } from '../../store/userSlice/types';
+
+type PropsType = {
+  user: UserState
+};
+
+export const Routes = ({ user }: PropsType) => {
+  /* hooks */
+  const location = useLocation();
+  const prevPath = location.state ? (location.state as any).prevPath : undefined;
+
+  const {
+    data: userData,
+    loading: isLoading,
+    orders,
+  } = user;
+  const { login, role } = userData;
+  const isLoggedIn = Boolean(login);
+
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to={routes.me.root} />
+      </Route>
+      <PrivateRoute
+        path={routes.me.root}
+        exact
+        condition={isLoggedIn}
+        redirectPath={routes.login.root}
+      >
+        <Me userData={userData} />
+      </PrivateRoute>
+      <PrivateRoute
+        path={routes.login.root}
+        exact
+        condition={!isLoggedIn}
+        redirectPath={prevPath || routes.me.root}
+      >
+        <LoginPage isLoading={isLoading} />
+      </PrivateRoute>
+      <PrivateRoute
+        path={routes.orders.root}
+        exact
+        condition={isLoggedIn}
+        redirectPath={routes.login.root}
+      >
+        <OrdersPage orders={orders} />
+      </PrivateRoute>
+      <PrivateRoute
+        path={routes.register.root}
+        exact
+        condition={!isLoggedIn}
+        redirectPath={routes.me.root}
+      >
+        <RegisterPage isLoading={isLoading} />
+      </PrivateRoute>
+      <PrivateRoute
+        path={routes.locations.root}
+        redirectPath={routes.login.root}
+        condition={isLoggedIn}
+        allowedRoles={[UserRole.ADMIN]}
+        role={role}
+      >
+        <LocationsPage />
+      </PrivateRoute>
+      <PrivateRoute
+        path={routes.services.root}
+        condition={isLoggedIn}
+        redirectPath={routes.login.root}
+        allowedRoles={[UserRole.ADMIN]}
+        role={role}
+      >
+        <ServicesPage />
+      </PrivateRoute>
+      <PrivateRoute
+        path={routes.specializations.root}
+        condition={isLoggedIn}
+        redirectPath={routes.login.root}
+        allowedRoles={[UserRole.ADMIN]}
+        role={role}
+      >
+        <SpecializationsPage />
+      </PrivateRoute>
+      <PrivateRoute
+        path={routes.schedule.root}
+        condition={isLoggedIn}
+        redirectPath={routes.login.root}
+        allowedRoles={[UserRole.ADMIN, UserRole.OPERATOR, UserRole.MASTER]}
+        role={role}
+      >
+        <MySchedulePage />
+      </PrivateRoute>
+      <Route path="*">Not found</Route>
+    </Switch>
+  );
+};
