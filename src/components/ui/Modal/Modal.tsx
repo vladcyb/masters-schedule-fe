807356b5ec109e85d38f82cc55129525e10e15db
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-03-12 19:22:12 +0300

    fix: Modal

--- src/components/ui/Modal/Modal.tsx
+++ src/components/ui/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { Button } from '../Button';
 import './style.css';
@@ -14,29 +14,47 @@ export const Modal = ({
   message,
   onConfirm,
   onClose,
-}: PropsType) => (
-  ReactDOM.createPortal(
-    <div className="modal">
-      <div className="modal__content">
-        <div className="modal__message">
-          {message}
-        </div>
-        {onConfirm ? (
-          <div className="modal__buttons">
-            <Button onClick={onConfirm}>OK</Button>
-            <Button onClick={onClose} variant="outline">Cancel</Button>
+}: PropsType) => {
+  /* methods */
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Escape' && onClose) {
+      onClose();
+    }
+  };
+
+  /* effects */
+  useEffect(() => {
+    window.addEventListener('keydown', onKeyDown);
+    return () => {
+      window.removeEventListener('keydown', onKeyDown);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    ReactDOM.createPortal(
+      <div className="modal">
+        <div className="modal__content">
+          <div className="modal__message">
+            {message}
           </div>
-        ) : (
-          <Button
-            className="modal__cancel"
-            variant="outline"
-            onClick={onClose}
-          >
-            Close
-          </Button>
-        )}
-      </div>
-    </div>,
-    document.getElementById('modal')!,
-  )
-);
+          {onConfirm ? (
+            <div className="modal__buttons">
+              <Button onClick={onConfirm}>OK</Button>
+              <Button onClick={onClose} variant="outline">Cancel</Button>
+            </div>
+          ) : (
+            <Button
+              className="modal__cancel"
+              variant="outline"
+              onClick={onClose}
+            >
+              Close
+            </Button>
+          )}
+        </div>
+      </div>,
+      document.getElementById('modal')!,
+    )
+  );
+};
--- src/pages/LocationsPage/LocationsPage.tsx
+++ src/pages/LocationsPage/LocationsPage.tsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { thunks } from '../../store/thunks';
 import { getLocations } from '../../store/locationSlice/selectors';
-import { Button, Spinner, Container } from '../../components/ui';
+import {
+  Button, Spinner, Container, Modal,
+} from '../../components/ui';
 import { useAppDispatch } from '../../store';
 import { Location, CreateLocationForm } from '../../components';
+import { getLocationById } from '../../store/locationSlice/methods';
 import './style.css';
 
 export const LocationsPage = () => {
@@ -14,6 +17,11 @@ export const LocationsPage = () => {
 
   /* state */
   const [isAdding, setIsAdding] = useState(false);
+  const [deletingLocation, setDeletingLocation] = useState<undefined | number>(undefined);
+  const [error, setError] = useState('');
+
+  const deletingLocationTitle = deletingLocation
+    ? getLocationById(locations.data, 0, deletingLocation)!.title : '';
 
   /* effects */
   useEffect(() => {
@@ -25,52 +33,78 @@ export const LocationsPage = () => {
     setIsAdding(true);
   };
 
+  const closeErrorModal = () => {
+    setError('');
+  };
+
+  const closeModal = () => {
+    setDeletingLocation(undefined);
+  };
+
   const cancelAdding = () => {
     setIsAdding(false);
   };
 
-  const handleDeleteLocation = async (id: number) => {
-    const result = await dispatch(thunks.location.delete(id));
+  const handleDeleteLocation = (id: number) => {
+    setDeletingLocation(id);
+  };
+
+  const deleteLocation = async () => {
+    if (!deletingLocation) {
+      return;
+    }
+    closeModal();
+    const result = await dispatch(thunks.location.delete(deletingLocation));
     if (result.meta.requestStatus === 'rejected') {
-      alert(result.payload);
+      setError(result.payload as string);
     }
   };
 
   return (
-    <Container className="locationsPage">
-      {isAdding ? (
-        <>
-          <div className="center title">Новое местоположение</div>
-          <CreateLocationForm
-            className="locationsPage__createForm"
-            close={cancelAdding}
-            locations={locations}
-            isLoading={locations.loading}
-          />
-        </>
-      ) : (
-        <>
-          <Spinner visible={locations.loading} />
-          {locations.data.map((item) => (
-            <Location
-              className="locationsPage__location"
-              data={item}
-              onDelete={handleDeleteLocation}
-              types={locations.types}
-              nestingDegree={1}
-              key={item.id}
+    <>
+      <Container className="locationsPage">
+        {isAdding ? (
+          <>
+            <div className="center title">Новое местоположение</div>
+            <CreateLocationForm
+              className="locationsPage__createForm"
+              close={cancelAdding}
+              locations={locations}
+              isLoading={locations.loading}
             />
-          ))}
-          {!locations.data.length && !locations.loading && (
-            <div>
-              <i>(пусто)</i>
-            </div>
-          )}
-          <Button className="locationsPage__createButton" onClick={handleAddClick}>
-            Создать
-          </Button>
-        </>
+          </>
+        ) : (
+          <>
+            <Spinner visible={locations.loading} />
+            {locations.data.map((item) => (
+              <Location
+                className="locationsPage__location"
+                data={item}
+                onDelete={handleDeleteLocation}
+                types={locations.types}
+                nestingDegree={1}
+                key={item.id}
+              />
+            ))}
+            {!locations.data.length && !locations.loading && (
+              <div>
+                <i>(пусто)</i>
+              </div>
+            )}
+            <Button className="locationsPage__createButton" onClick={handleAddClick}>
+              Создать
+            </Button>
+          </>
+        )}
+      </Container>
+      {deletingLocation && (
+        <Modal
+          message={`Вы уверены, что хотите удалить местоположение "${deletingLocationTitle}"?`}
+          onClose={closeModal}
+          onConfirm={deleteLocation}
+        />
       )}
-    </Container>
+      {error && <Modal message={error} onClose={closeErrorModal} />}
+    </>
   );
 };
