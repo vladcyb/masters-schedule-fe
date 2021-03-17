b108ac245a58418d212bad6e26cb39fb877e354f
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-03-17 19:12:44 +0300

    refactor: мелкая правка

--- src/pages/AdministrationPage/AdministrationPage.tsx
+++ src/pages/AdministrationPage/AdministrationPage.tsx
@@ -4,28 +4,25 @@ import { Container } from '../../components/ui';
 import { routes } from '../../shared/routes';
 import './style.css';
 
-export const AdministrationPage = () => {
-  console.log('AdministrationPage');
-  return (
-    <Container className="administrationPage">
-      <div className="title">Администрирование</div>
-      <ul className="administrationPage__navigation">
-        <li>
-          <Link className="navlink" to={routes.administration.locations.root}>
-            Местоположения
-          </Link>
-        </li>
-        <li>
-          <Link className="navlink" to={routes.administration.specializations.root}>
-            Специализации
-          </Link>
-        </li>
-        <li>
-          <Link className="navlink" to={routes.administration.services.root}>
-            Услуги
-          </Link>
-        </li>
-      </ul>
-    </Container>
-  );
-};
+export const AdministrationPage = () => (
+  <Container className="administrationPage">
+    <div className="title">Администрирование</div>
+    <ul className="administrationPage__navigation">
+      <li>
+        <Link className="navlink" to={routes.administration.locations.root}>
+          Местоположения
+        </Link>
+      </li>
+      <li>
+        <Link className="navlink" to={routes.administration.specializations.root}>
+          Специализации
+        </Link>
+      </li>
+      <li>
+        <Link className="navlink" to={routes.administration.services.root}>
+          Услуги
+        </Link>
+      </li>
+    </ul>
+  </Container>
+);
