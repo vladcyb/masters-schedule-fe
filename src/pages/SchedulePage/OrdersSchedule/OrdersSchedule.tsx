af1cbf2e547bcac6d0d298e2fc0cd458bdbad10e
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-04-07 17:35:52 +0300

    refactor: Поднял выбор даты наверх (по структуре компонентов)

--- src/pages/SchedulePage/OrdersSchedule/OrdersSchedule.tsx
+++ src/pages/SchedulePage/OrdersSchedule/OrdersSchedule.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { addDays, format, parseISO } from 'date-fns';
-import { dateFormat, hours } from '../../../shared/constants';
+import React, { useEffect } from 'react';
+import { hours } from '../../../shared/constants';
 import { ScheduleRow } from './__row';
 import { useAppDispatch } from '../../../store';
 import { thunks } from '../../../store/thunks';
@@ -9,63 +8,42 @@ import './style.css';
 
 type PropsType = {
   orders: OrderType[]
+  selectedDate: string
 };
 
-export const OrdersSchedule = ({ orders }: PropsType) => {
+export const OrdersSchedule = ({ orders, selectedDate }: PropsType) => {
   /* hooks */
   const dispatch = useAppDispatch();
 
-  /* state */
-  const [selectedDate, setSelectedDate] = useState(format(new Date(), dateFormat));
-
   /* methods */
-  const decrementDate = () => {
-    setSelectedDate((oldValue) => format(addDays(parseISO(oldValue), -1), dateFormat));
-  };
-
-  const incrementDate = () => {
-    setSelectedDate((oldValue) => format(addDays(parseISO(oldValue), 1), dateFormat));
-  };
-
   useEffect(() => {
     dispatch(thunks.order.get());
   }, [dispatch]);
 
   return (
-    <div className="ordersSchedule">
-      <div className="ordersSchedule__date">
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button className="ordersSchedule__datePrev" onClick={decrementDate} type="button" />
-        <div className="ordersSchedule__dateValue">
-          {selectedDate}
-        </div>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button className="ordersSchedule__dateNext" onClick={incrementDate} type="button" />
-      </div>
-      <table className="ordersSchedule__table">
-        <tbody>
-          <tr className="ordersSchedule__tr">
-            <td className="ordersSchedule__td">
-              Наименование заказа
+    <table className="ordersSchedule">
+      <tbody>
+        <tr className="ordersSchedule__tr">
+          <td className="ordersSchedule__td">
+            Наименование заказа
+          </td>
+          {hours.map((hour) => (
+            <td className="ordersSchedule__td" key={hour}>
+              {hour}
+              :00
             </td>
-            {hours.map((hour) => (
-              <td className="ordersSchedule__td" key={hour}>
-                {hour}
-                :00
-              </td>
-            ))}
-          </tr>
-          {orders.map((order) => (
-            <ScheduleRow
-              id={order.id}
-              startDate={order.startDate}
-              finishDate={order.finishDate}
-              selectedDate={selectedDate}
-              key={order.id}
-            />
           ))}
-        </tbody>
-      </table>
-    </div>
+        </tr>
+        {orders.map((order) => (
+          <ScheduleRow
+            id={order.id}
+            startDate={order.startDate}
+            finishDate={order.finishDate}
+            selectedDate={selectedDate}
+            key={order.id}
+          />
+        ))}
+      </tbody>
+    </table>
   );
 };
--- src/pages/SchedulePage/SchedulePage.tsx
+++ src/pages/SchedulePage/SchedulePage.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { addDays, format, parseISO } from 'date-fns';
 import { Container } from '../../components/ui';
 import { OrderType } from '../../shared/types';
 import { OrdersSchedule } from './OrdersSchedule';
+import { dateFormat } from '../../shared/constants';
 import './style.css';
 
 type PropsType = {
   orders: OrderType[]
 };
 
-export const SchedulePage = ({ orders }: PropsType) => (
-  <Container>
-    <OrdersSchedule orders={orders} />
-  </Container>
-);
+export const SchedulePage = ({ orders }: PropsType) => {
+  /* state */
+  const [selectedDate, setSelectedDate] = useState<string>(format(new Date(), dateFormat));
+
+  /* methods */
+  const decrementDate = () => {
+    setSelectedDate((oldValue) => format(addDays(parseISO(oldValue), -1), dateFormat));
+  };
+
+  const incrementDate = () => {
+    setSelectedDate((oldValue) => format(addDays(parseISO(oldValue), 1), dateFormat));
+  };
+
+  return (
+    <Container>
+      <div className="schedulePage__date">
+        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+        <button className="schedulePage__datePrev" onClick={decrementDate} type="button" />
+        <div className="schedulePage__dateValue">
+          {selectedDate}
+        </div>
+        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+        <button className="schedulePage__dateNext" onClick={incrementDate} type="button" />
+      </div>
+      <OrdersSchedule orders={orders} selectedDate={selectedDate} />
+    </Container>
+  );
+};
