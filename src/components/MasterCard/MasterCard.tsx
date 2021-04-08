97c249a960d8df0834be11b2f7452c44575010fe
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-04-08 13:46:19 +0300

    fix: Верстка в карточке мастера

--- src/components/MasterCard/MasterCard.tsx
+++ src/components/MasterCard/MasterCard.tsx
@@ -8,25 +8,43 @@ type PropsType = {
   master: MasterType
 };
 
-// TODO: редактирование специализаций
 export const MasterCard = ({
   className,
   master,
-}: PropsType) => (
-  <Card className={`masterCard ${className || ''}`} key={master.id}>
-    <div>
-      <span className="masterCard__fieldName">ФИО: </span>
-      <span className="masterCard__fieldContent">
-        {`${master.user.surname} ${master.user.name} ${master.user.patronymic}`}
-      </span>
-    </div>
-    <div>
-      <span className="masterCard__fieldName">Специализации: </span>
-      <span className="masterCard__fieldContent">
-        {master.specializations.map((specialization) => (
-          `${specialization.title}; `
-        ))}
-      </span>
-    </div>
-  </Card>
-);
+}: PropsType) => {
+  /* methods */
+  const handleDeleteSpecializationClick = () => {
+    console.log(123123132132);
+  };
+
+  return (
+    <Card className={`masterCard ${className || ''}`} key={master.id}>
+      <div>
+        <span className="masterCard__fieldName">ФИО: </span>
+        <span className="masterCard__fieldContent">
+          {`${master.user.surname} ${master.user.name} ${master.user.patronymic}`}
+        </span>
+      </div>
+      <div>
+        <span className="masterCard__fieldName">Специализации: </span>
+        <span className="masterCard__fieldContent">
+          {master.specializations.map((specialization) => (
+            <div className="masterCard__specialization" key={specialization.id}>
+              <div className="masterCard__specializationWrapper">
+                <div className="masterCard__specializationTitle">
+                  {specialization.title}
+                </div>
+                {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+                <button
+                  className="masterCard__deleteSpecialization"
+                  onClick={handleDeleteSpecializationClick}
+                  type="button"
+                />
+              </div>
+            </div>
+          ))}
+        </span>
+      </div>
+    </Card>
+  );
+};
